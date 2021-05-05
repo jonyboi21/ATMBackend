c771db921516531a4fe285a79785bd455c64e67a
jonyboi21 <jmakori@github.com>
2021-05-04 22:03:55 -0400

    Added mysql database & rough draft of account pojo

---
+++ src/main/java/com/atm/atmproject/models/Account.java
@@ -0,0 +1,81 @@
+package com.atm.atmproject.models;
+
+import javax.persistence.*;
+
+@Entity
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
+    @Column(name = "ACCOUNT_ID")
+    private Long id;
+
+    @Column(name = "type")
+    private Enum type;
+
+    @Column(name = "NICKNAME")
+    private String nickname;
+
+    @Column(name = "REWARDS")
+    private Integer rewards;
+
+    @Column(name = "BALANCE")
+    private Double balance;
+
+    @OneToOne
+    private Customer customer;
+
+    public Account() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public Integer getRewards() {
+        return rewards;
+    }
+
+    public void setRewards(Integer rewards) {
+        this.rewards = rewards;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", nickname='" + nickname + '\'' +
+                ", rewards=" + rewards +
+                ", balance=" + balance +
+                ", customer=" + customer +
+                '}';
+    }
+}
