26a5aa9ecf8e898dcef18a301b0f3dfe9e7c781e
Jacenter <JNovack1501@gmail.com>
2021-05-07 15:49:10 -0400

    cleaned up and fixed deposit POJO, controller, service and repository

--- src/main/java/com/atm/atmproject/repositories/DepositRepository.java
+++ src/main/java/com/atm/atmproject/repositories/DepositRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface DepositRepository extends CrudRepository <Deposit, Long> {
 
-    Iterable<Deposit>
+    Iterable<Deposit> findAllDepositsByAccountId(Long accountId);
 }
