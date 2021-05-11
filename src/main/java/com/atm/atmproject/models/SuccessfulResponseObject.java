3788aa7ad00511578b4e8e423e4926c14be33fad
KameronThomas00 <kthomas@wil.yearup.org>
2021-05-11 16:24:32 -0400

    Looger for BILLS and SUccessful responses

--- src/main/java/com/atm/atmproject/models/SuccessfulResponseObject.java
+++ src/main/java/com/atm/atmproject/models/SuccessfulResponseObject.java
@@ -1,7 +1,10 @@
 package com.atm.atmproject.models;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.util.Optional;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class SuccessfulResponseObject {
     private int code;
     private String message;
