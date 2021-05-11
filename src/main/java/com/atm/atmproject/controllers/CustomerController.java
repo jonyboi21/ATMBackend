7fbb746e7c3ea5647d5669cb8392856f27f2aaaa
Jacenter <JNovack1501@gmail.com>
2021-05-11 14:20:30 -0400

    working on successful responses

--- src/main/java/com/atm/atmproject/controllers/CustomerController.java
+++ src/main/java/com/atm/atmproject/controllers/CustomerController.java
@@ -2,8 +2,6 @@ package com.atm.atmproject.controllers;
 import com.atm.atmproject.models.Customer;
 import com.atm.atmproject.models.SuccessfulResponse;
 import com.atm.atmproject.services.CustomerService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,8 +13,6 @@ import java.util.Optional;
 @RestController
 public class CustomerController {
 
-    private static final Logger customerLogger = LoggerFactory.getLogger(CustomerController.class);
-
     @Autowired
     private CustomerService customerService;
 
@@ -27,7 +23,6 @@ public class CustomerController {
         customerService.verifyCustomersInRepository();
         Iterable<Customer> getAllCustomers = customerService.getAllCustomers();
         SuccessfulResponse successfulResponse = new SuccessfulResponse(HttpStatus.OK.value(), "Success", customerService.getAllCustomers());
-        customerLogger.info("SUCCESSFULLY RETRIEVED ALL CUSTOMERS");
         return new ResponseEntity<Object>(successfulResponse, HttpStatus.OK);
     }
 
@@ -37,7 +32,6 @@ public class CustomerController {
         customerService.verifyCustomer(customerId);
         Optional<Customer> getCustomerById = customerService.getCustomerById(customerId);
         SuccessfulResponse successfulResponse = new SuccessfulResponse(HttpStatus.OK.value(), "Success", getCustomerById);
-        customerLogger.info("SUCCESSFULLY RETRIEVED CUSTOMER WITH ID OF " + customerId);
         return new ResponseEntity<Object>(successfulResponse, HttpStatus.OK);
     }
 
@@ -46,7 +40,6 @@ public class CustomerController {
     public ResponseEntity<?> createCustomer(@Validated @RequestBody Customer customer) {
         customerService.createCustomer(customer);
         SuccessfulResponse successfulResponse = new SuccessfulResponse(HttpStatus.OK.value(), "Customer account updated", Optional.ofNullable(getCustomerById(customer.getCustomerId()).getBody()));
-        customerLogger.info("CUSTOMER WITH ID OF " + customer.getCustomerId() + " SUCCESSFULLY CREATED");
         return new ResponseEntity<Object>(successfulResponse, HttpStatus.CREATED);
     }
 
--- src/main/java/com/atm/atmproject/services/CustomerService.java
+++ src/main/java/com/atm/atmproject/services/CustomerService.java
@@ -5,6 +5,8 @@ import com.atm.atmproject.models.Customer;
 import com.atm.atmproject.models.SuccessfulResponse;
 import com.atm.atmproject.models.SuccessfulResponseWrapper;
 import com.atm.atmproject.repositories.CustomerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +14,8 @@ import java.util.Optional;
 
 @Service
 public class CustomerService {
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomerService.class);
 
     @Autowired
     private CustomerRepository customerRepository;
@@ -22,27 +26,32 @@ public class CustomerService {
         if (customerRepository.findAll() == null) {
             throw new ResourceNotFoundException("error");
         } else {
+            logger.info("SUCCESSFULLY RETRIEVED ALL CUSTOMERS");
             return customerRepository.findAll();
         }
     }
 
     //get a customer by Id
     public Optional<Customer> getCustomerById(Long customerId) {
+        logger.info("SUCCESSFULLY RETRIEVED CUSTOMER WITH ID OF " + customerId);
         return customerRepository.findById(customerId);
     }
 
     //create a customer
     public void createCustomer(Customer customer) {
+        logger.info("CUSTOMER SUCCESSFULLY CREATED");
         customerRepository.save(customer);
     }
 
     //update a customer
     public void updateCustomer(Customer customer, Long customerId) {
+        logger.info("CUSTOMER ACCOUNT FOR CUSTOMER " + customer.getCustomerId() + " SUCCESSFULLY UPDATED");
         customerRepository.save(customer);
     }
 
     //delete a customer
     public void deleteCustomer(Long customerId) {
+        logger.info("CUSTOMER " + customerId + " REMOVED FROM SYSTEM");
         customerRepository.deleteById(customerId);
     }
 
